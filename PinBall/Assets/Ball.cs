1ea0178a102f20e433bf3e9a1e12ee15a5f26a0e
Solobolt <solobolt@gmail.com>
2015-10-12 21:29:56 +1000

    Added Floor Material & Camera to the ball for later tilting

--- PinBall/Assets/Ball.cs
+++ PinBall/Assets/Ball.cs
@@ -5,15 +5,25 @@ public class Ball : MonoBehaviour {
 
 	public GameObject ball;
 	public GameObject spawnLocation;
+	private int tiltPower = 200;
+	private Camera mainCamera;
 
 	// Use this for initialization
 	void Start () {
-	
+		mainCamera = Camera.FindObjectOfType <Camera>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+	if (Input.GetKeyDown ("q"))
+		{
+			rigidbody.AddForce (new Vector3(-tiltPower,0,0));
+		}
+
+		if (Input.GetKeyDown ("e"))
+		{
+			rigidbody.AddForce (new Vector3(tiltPower,0,0));
+		}
 	}
 
 	void OnTriggerEnter(Collider coll)
